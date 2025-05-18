hexagon-geo-surv/tensorflow

Internal change
PiperOrigin-RevId: 759800545

tensorflow/core/data/utils_test.cc
@@ -17,6 +17,7 @@ limitations under the License.
 #include <vector>
 
 #include <gtest/gtest.h>
+#include "tensorflow/core/data/tf_data_file_logger_options.h"
 
 namespace tensorflow::data {
 namespace {
@@ -56,9 +57,9 @@ TEST(LocalityOptimizedPath, TfDataPath) {
 }
 
 TEST(LocalityOptimizedPath, LogFilenames) {
-  EXPECT_NO_FATAL_FAILURE(LogFilenames(
-      std::vector<std::string>({"/path/file1", "file2.txt", "a"})));
-  EXPECT_NO_FATAL_FAILURE(LogFilenames(std::vector<std::string>({})));
+  LogFilenamesOptions options({"/path/file1", "file2.txt", "a"}, "");
+  EXPECT_NO_FATAL_FAILURE(LogFilenames(options));
+  EXPECT_NO_FATAL_FAILURE(LogFilenames(options));
 }
 
 }  // namespace

tensorflow/core/data/tf_data_file_logger_options.h
@@ -1,0 +1,48 @@
+/* Copyright 2025 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_CORE_DATA_TF_DATA_FILE_LOGGER_OPTIONS_H_
+#define TENSORFLOW_CORE_DATA_TF_DATA_FILE_LOGGER_OPTIONS_H_
+
+#include <string>
+#include <vector>
+
+namespace tensorflow {
+namespace data {
+
+struct LogFilenamesOptions {
+  std::vector<std::string> files;
+  std::string data_service_address;
+
+  LogFilenamesOptions() : data_service_address("") {}
+  LogFilenamesOptions(const std::vector<std::string>& files,
+                      const std::string& data_service_address)
+      : files(files), data_service_address(data_service_address) {}
+};
+
+inline bool operator==(const LogFilenamesOptions& lhs,
+                       const LogFilenamesOptions& rhs) {
+  return lhs.files == rhs.files &&
+         lhs.data_service_address == rhs.data_service_address;
+}
+
+inline bool operator!=(const LogFilenamesOptions& lhs,
+                       const LogFilenamesOptions& rhs) {
+  return !(lhs == rhs);
+}
+
+}  // namespace data
+}  // namespace tensorflow
+
+#endif  // TENSORFLOW_CORE_DATA_TF_DATA_FILE_LOGGER_OPTIONS_H_

cns-test-github/tensorflow_fork

tensorflow/core/data/utils_test.cc
@@ -17,6 +17,7 @@ limitations under the License.
 #include <vector>
 
 #include <gtest/gtest.h>
+#include "tensorflow/core/framework/tf_data_file_logger_options.h"
 
 namespace tensorflow::data {
 namespace {
@@ -56,9 +57,9 @@ TEST(LocalityOptimizedPath, TfDataPath) {
 }
 
 TEST(LocalityOptimizedPath, LogFilenames) {
-  EXPECT_NO_FATAL_FAILURE(LogFilenames(
-      std::vector<std::string>({"/path/file1", "file2.txt", "a"})));
-  EXPECT_NO_FATAL_FAILURE(LogFilenames(std::vector<std::string>({})));
+  LogFilenamesOptions options({"/path/file1", "file2.txt", "a"}, "");
+  EXPECT_NO_FATAL_FAILURE(LogFilenames(options));
+  EXPECT_NO_FATAL_FAILURE(LogFilenames(options));
 }
 
 }  // namespace

tensorflow/core/framework/tf_data_file_logger_options.h
@@ -1,0 +1,48 @@
+/* Copyright 2025 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_CORE_FRAMEWORK_TF_DATA_FILE_LOGGER_OPTIONS_H_
+#define TENSORFLOW_CORE_FRAMEWORK_TF_DATA_FILE_LOGGER_OPTIONS_H_
+
+#include <string>
+#include <vector>
+
+namespace tensorflow {
+namespace data {
+
+struct LogFilenamesOptions {
+  std::vector<std::string> files;
+  std::string data_service_address;
+
+  LogFilenamesOptions() : data_service_address("") {}
+  LogFilenamesOptions(const std::vector<std::string>& files,
+                      const std::string& data_service_address)
+      : files(files), data_service_address(data_service_address) {}
+};
+
+inline bool operator==(const LogFilenamesOptions& lhs,
+                       const LogFilenamesOptions& rhs) {
+  return lhs.files == rhs.files &&
+         lhs.data_service_address == rhs.data_service_address;
+}
+
+inline bool operator!=(const LogFilenamesOptions& lhs,
+                       const LogFilenamesOptions& rhs) {
+  return !(lhs == rhs);
+}
+
+}  // namespace data
+}  // namespace tensorflow
+
+#endif  // TENSORFLOW_CORE_FRAMEWORK_TF_DATA_FILE_LOGGER_OPTIONS_H_